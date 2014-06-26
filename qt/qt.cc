d88164c5245c5761193b301544f0cb347afbdc8b
tkatchev <tkatchev@gmail.com>
2014-06-26 09:42:38 +0400

    .

---
+++ qt/qt.cc
@@ -0,0 +1,119 @@
+
+#include <QtGui>
+
+#include <unordered_map>
+#include <vector>
+#include <string>
+
+#include "bdf.h"
+#include "config.h"
+
+class RasterWindow : public QWindow {
+
+//    Q_OBJECT
+
+public:
+
+    config::Config& cfg;
+
+    bdf::Font font;
+    
+    explicit RasterWindow(config::Config& _cfg, QWindow* parent = 0) : 
+        QWindow(parent), cfg(_cfg), m_update_pending(false) {
+
+        bdf::parse_bdf(cfg.fonts.front(), font);
+
+        m_backingStore = new QBackingStore(this);
+        create();
+
+        setGeometry(100, 100, 300, 200);
+    }
+
+    virtual void render(QPainter* painter) {
+        //painter->drawText(QRectF(0, 0, width(), height()), Qt::AlignCenter, QStringLiteral("QWindow"));
+
+        QColor bgc(0, 0, 0);
+        QColor fgc(0, 0xFF, 0xFF);
+
+        painter->fillRect(50, 50, 8, 16, bgc);
+
+        const auto& glyph = font.glyphs['&'];
+
+        QBitmap bm = QBitmap::fromData(QSize(glyph.w, font.h), &(glyph.bm[0]), QImage::Format_Mono);
+
+        painter->drawPixmap(58, 50, bm);
+    }
+
+public slots:
+
+    void renderLater() {
+
+        if (!m_update_pending) {
+            m_update_pending = true;
+            QCoreApplication::postEvent(this, new QEvent(QEvent::UpdateRequest));
+        }
+    }
+
+    void renderNow() {
+
+        if (!isExposed())
+            return;
+
+        QRect rect(0, 0, width(), height());
+        m_backingStore->beginPaint(rect);
+
+        QPaintDevice *device = m_backingStore->paintDevice();
+        QPainter painter(device);
+
+        painter.fillRect(0, 0, width(), height(), Qt::white);
+        render(&painter);
+
+        m_backingStore->endPaint();
+        m_backingStore->flush(rect);
+    }
+
+protected:
+
+    bool event(QEvent* event) {
+
+        if (event->type() == QEvent::UpdateRequest) {
+            m_update_pending = false;
+            renderNow();
+            return true;
+        }
+        return QWindow::event(event);
+    }
+
+    void resizeEvent(QResizeEvent* event) {
+
+        m_backingStore->resize(event->size());
+        if (isExposed())
+            renderNow();
+    }
+
+    void exposeEvent(QExposeEvent* event) {
+
+        if (isExposed()) {
+            renderNow();
+        }
+    }
+
+private:
+
+    QBackingStore* m_backingStore;
+    bool m_update_pending;
+};
+
+
+int main(int argc, char** argv) {
+
+    config::Config cfg;
+    config::parse_config("default.cfg", cfg);
+
+    QGuiApplication app(argc, argv);
+
+    RasterWindow window(cfg);
+    window.show();
+
+    return app.exec();
+}
